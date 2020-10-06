bfa02d85cf7659d7176f2464bcb486e022c23877
andrea <andrearaiatea@gmail.com>
2020-10-06 15:02:49 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public pseudo ="";
+  public niveau="";
+  public sauvegarder = false;
+  public error="";
+  public startGame = false;
+  public nextQuest = false;
+
 
   constructor() {}
+
+  public commencer(){
+    if((this.pseudo === "" || this.pseudo.length < 3) || this.niveau === ""){
+      this.error = "Veuillez entrer un pseudo et un niveau";
+      return;
+    }
+    this.startGame = true;
+  }
+
+  public response(response:string){
+    this.nextQuest = true;
+  }
 
 }
